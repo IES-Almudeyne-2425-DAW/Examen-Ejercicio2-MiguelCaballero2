39c9902402db31b79460eea13b7cf720b1524877
migcabvid <miguelcaballerovidal@gmail.com>
2024-11-06 10:01:25 +0100

    intento defecto

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package Ejercicio 2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo en la rama login");
-		System.out.println("Hola Mundo en la rama funcionalidad");
+		System.out.println("Hola Mundo en la rama main");
+		System.out.println("Hola Mundo en la rama main");
 	}
 }
