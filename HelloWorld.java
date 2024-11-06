7e0fa969570f3f2361ca5cc1ace9a20363e9929e
migcabvid <miguelcaballerovidal@gmail.com>
2024-11-06 10:02:27 +0100

    modificacion main

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package Ejercicio 2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo en la rama main");
-		System.out.println("Hola Mundo en la rama main");
+		System.out.println("Hola Mundo en main");
+		System.out.println("Hola Mundo en main");
 	}
 }
