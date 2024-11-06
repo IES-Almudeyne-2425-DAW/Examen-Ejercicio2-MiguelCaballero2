46942cace3b693284a2bcae46b4e1783e25fea76
migcabvid <miguelcaballerovidal@gmail.com>
2024-11-06 09:56:10 +0100

    modificado java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package Ejercicio 2;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo en la rama login");
-		System.out.println("Hola Mundo en la rama login");
+		System.out.println("Hola Mundo en la rama funcionalidad");
 	}
 }
